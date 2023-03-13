8a684aef5111434b9e426606c3a6371fb33a7ff4
Rannar Randmäe <rannra@ttu.ee>
2023-03-13 23:39:08 +0200

    Form updated, opens new window when correct password. User: Pets, PW:1234 for testing

--- FormLogin/FormLogin.cpp
+++ FormLogin/FormLogin.cpp
@@ -1,4 +1,5 @@
 #include "FormLogin.h"
+#include "FormMessenger.h"
 
 FormLogin::FormLogin(QWidget *parent)
     : QMainWindow(parent)
@@ -8,3 +9,20 @@ FormLogin::FormLogin(QWidget *parent)
 
 FormLogin::~FormLogin()
 {}
+
+void FormLogin::on_btnLogin_clicked()
+{
+    QString strUserName = ui.lineEditUsername->text();
+    QString strPassWord = ui.lineEditPassword->text();
+    if (strUserName == "Pets" && strPassWord == "1234")
+    {
+        hide();
+        formMessenger = new FormMessenger(this);
+        formMessenger->show();
+    }
+    else
+    {
+        ui.statusBar->showMessage("Vale kasutajanimi voi salasona",3000);
+    }
+    
+}
