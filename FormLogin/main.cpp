ea1a24bcf19d77b4555ccf2be8d6b5ab9b95512e
Rannar Randmäe <rannra@ttu.ee>
2023-03-14 02:27:20 +0200

    Ui update

--- FormLogin/main.cpp
+++ FormLogin/main.cpp
@@ -1,10 +1,13 @@
 #include "FormLogin.h"
 #include <QtWidgets/QApplication>
 
+
+
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    FormLogin w;
-    w.show();
+    FormLogin *formLogin = new FormLogin;
+
+    formLogin->show();
     return a.exec();
 }
