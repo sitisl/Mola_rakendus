d65ade53873b5ec4d9b577bac551225a30b67026
Rannar Randmäe <rannra@ttu.ee>
2023-03-26 13:56:48 +0300

    Added files

--- FormLogin/main.cpp
+++ FormLogin/main.cpp
@@ -1,11 +1,23 @@
 #include "FormLoginDiag.h"
 #include <QtWidgets/QApplication>
+#include <QFile>
 
 
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+    QFile f(":/qdarkstyle/theme/darkstyle.qss");
+    if (!f.exists())
+    {
+        printf("Unable to set stylesheet. File not found!\n");
+    }
+    else
+    {
+        f.open(QFile::ReadOnly | QFile::Text);
+		QTextStream ts(&f);
+		a.setStyleSheet(ts.readAll());
+    }
     FormLoginDiag *formLogin = new FormLoginDiag();
     formLogin->show();
     return a.exec();
