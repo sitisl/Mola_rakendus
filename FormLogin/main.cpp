e5bf910e8600749019e164b0197241b415d64b0b
Rannar Randmäe <rannra@ttu.ee>
2023-03-26 16:19:09 +0300

    Dark theme applied

--- FormLogin/main.cpp
+++ FormLogin/main.cpp
@@ -7,7 +7,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    QFile f(":/qdarkstyle/theme/darkstyle.qss");
+    QFile f(":/qdarkstyle/qdarkstyle/theme/darkstyle.qss");
     if (!f.exists())
     {
         printf("Unable to set stylesheet. File not found!\n");
