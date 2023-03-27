2b173f823d01123949dcc7ebd9c846a4bc698518
Rannar Randmäe <rannra@ttu.ee>
2023-03-27 18:26:06 +0300

    More modern theme(dark mode). Added enter press to login, enter press to send a message.

--- FormLogin/FormName.cpp
+++ FormLogin/FormName.cpp
@@ -1,11 +1,15 @@
 #include "FormName.h"
 #include "FormMessenger.h"
+#include <QShortcut>
 
 FormName::FormName(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
     setWindowIcon(QIcon(":/myresources/icons/Mola.png"));
+    //Create shortcut for return key, to login when pressing enter.
+    QShortcut *shortcut = new QShortcut(QKeySequence(Qt::Key_Return), this);
+    QObject::connect(shortcut, SIGNAL(activated()), ui.btnJatka, SLOT(click()));
 }
 
 FormName::~FormName()
--- FormLogin/main.cpp
+++ FormLogin/main.cpp
@@ -1,12 +1,22 @@
 #include "FormName.h"
 #include <QtWidgets/QApplication>
+#include <QFile>
 
 int main(int argc, char *argv[])
 {
     Q_INIT_RESOURCE(resources);
-    QFont amazingFont("Consolas", 8, QFont::Monospace, true);
-    QApplication::setFont(amazingFont);
+    //QFont amazingFont("Consolas", 8, QFont::Monospace, true);
+    //QApplication::setFont(amazingFont);
     QApplication application(argc, argv);
+    QFile f(":myresources/themes/darkstyle.qss");
+    if (!f.exists()) {
+        printf("Unable to set stylesheet, file not found\n");
+    }
+    else {
+        f.open(QFile::ReadOnly | QFile::Text);
+        QTextStream ts(&f);
+        application.setStyleSheet(ts.readAll());
+    }
     FormName formName;
     formName.show();
 
